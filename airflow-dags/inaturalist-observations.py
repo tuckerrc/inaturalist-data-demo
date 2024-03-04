4a96d25f3fef368759e400b9256dd8198bed242f
Tucker Chapman <tucker.r.chapman@gmail.com>
2024-03-04 11:09:43 -0500

    Set airflow dag to run daily

--- airflow-dags/inaturalist-observations.py
+++ airflow-dags/inaturalist-observations.py
@@ -33,6 +33,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
+    'schedule_interval': "@daily",
 }
 with DAG(
     'inaturalist-observations',
